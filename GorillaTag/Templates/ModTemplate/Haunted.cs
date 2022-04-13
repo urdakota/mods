7826b93c26244bc90da7ef2ba937ebe3d04e395f
Dakota <95160810+urdakota@users.noreply.github.com>
2022-04-13 10:47:08 -0400

    Update Haunted.cs

--- GorillaTag/Templates/ModTemplate/Haunted.cs
+++ GorillaTag/Templates/ModTemplate/Haunted.cs
@@ -2,10 +2,12 @@ using System.ComponentModel;
 
 namespace Project
 {
-    [Description("HauntedModMenu")] 
+    [Description("HauntedModMenu")]  // Required for HMM
+
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
     [ModdedGamemode]
+    
     public class ConfigablePlugin : BaseUnityPlugin
     {
         public static bool allowed = false;
@@ -15,6 +17,8 @@ namespace Project
         {
             Patches.ApplyHarmonyPatches();
 
+	    // CONFIGS
+	    
             var customFile = new ConfigFile(Path.Combine(Paths.ConfigPath, "SpaceMonke.cfg"), true);
             multiplier = customFile.Bind("Configuration", "JumpMultiplier", 10f, "How much to multiply the jump height/distance by. 10 = 10x higher jumps");
         }
