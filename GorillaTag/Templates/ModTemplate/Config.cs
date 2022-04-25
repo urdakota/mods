3885b95d2535596b11b5ab75b85b82e7000276dc
Dakota <95160810+urdakota@users.noreply.github.com>
2022-04-25 00:02:26 -0400

    Delete Config.cs

--- GorillaTag/Templates/ModTemplate/Config.cs
+++
@@ -1,8 +0,0 @@
-
-using BepInEx.Configuration;
-
-// Variable
-ConfigEntry<float> bind;
-
-var customFile = new ConfigFile(Path.Combine(Paths.ConfigPath, "NAME.cfg"), true);
-bind = customFile.Bind("CATEGORY", "NAME",1f /* Default */, "DESCRIPTION");
